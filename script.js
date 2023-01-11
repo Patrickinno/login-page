129577868600172c1e7ffcdb18e7bab7b90b97ee
Patrickinno <114596018+Patrickinno@users.noreply.github.com>
2023-01-11 00:37:56 -0800

    Add files via upload
    I created a login page with a sliding function.

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const signUpBtn = document.querySelector(".blue-bgr button");
+
+signUpBtn.addEventListener("click",
+() => {
+    container.classList.toggle("change");
+}
+);
+Footer
